393ca1d5331dfc68e27b3716eada484da58d212d
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-11-10 09:37:58 +0100

    :fire::fire::fire: initial commiit

---
+++ README.md
@@ -0,0 +1,18 @@
+# alc-studentrecord🔥🔥🔥
+
+> student record frontend for alc project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev🔥
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+           <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title> Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>person</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Add Student</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>people</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>View All Students</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>ALC Student Records</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-tooltip right>
+              <v-btn icon large :href="source" target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer app fixed class="text-center">
+      <span>&copy; Ipaye Alameen 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
